9d734ed2d31c93853016acef92cc00a3ebf1e324
Marty Ulrich <martyulrich@earthlink.net>
2012-03-11 20:15:41 -0700

    Initial Commit

---
+++ compute/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  compute
+//
+//  Created by Marty Ulrich on 3/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
